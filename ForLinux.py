5ad2552daf7634baed68113afe5f48d70e05608b
Joshua <jaswafford1211@gmail.com>
2021-08-23 16:38:43 -0500

    Create ForLinux.py

---
+++ ForLinux.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+import pyautogui
+import time
+pyautogui.hotkey("alt","f1")
+time.sleep(0.5)
+pyautogui.hotkey("down")
+time.sleep(0.5)
+pyautogui.hotkey("down")
+time.sleep(0.5)
+pyautogui.hotkey("down")
+time.sleep(0.5)
+pyautogui.hotkey("down")
+time.sleep(0.5)
+pyautogui.hotkey("right")
+time.sleep(0.5)
+pyautogui.hotkey("enter")
+time.sleep(0.01)
+
+print("Done")
+time.sleep(0.01)
